da760226c05884630e30061923d33a4e71a54e5a
Jared Updike <jupdike@thebrain.com>
2025-02-23 13:02:26 -0800

    Try turning rectangles into Register objects. Mostly kind of works?
    SSAsset code copied from Ainter / SnapScale functionality

--- Mochi Diffusion/Model/MDProject.swift
+++ Mochi Diffusion/Model/MDProject.swift
@@ -21,6 +21,112 @@ import Vision
 //}
 // }
 
+struct Register: Identifiable, Equatable, Hashable {
+    let scale: Double
+    let offsetX: Int
+    let offsetY: Int
+    let nw: Int
+    let nh: Int
+    let id: UUID = UUID()
+}
+
+public struct SSAsset: Identifiable, Equatable, Hashable {
+    var result: Register?
+    // needs to be converted to result relative to parent, not relative to base
+    var rectToBase: CGRect?
+    var image: CGImage?
+    public var id: UUID
+    var parentId: UUID?
+
+    var isBaseImage: Bool { parentId == nil }
+
+    public let stage: AssetStage
+
+    init(
+        image: CGImage? = nil,
+        result: Register? = nil,
+        rectToBase: CGRect? = nil,
+        parentId: UUID? = nil,
+        stage: AssetStage? = nil,
+        id: UUID? = nil
+    ) {
+        self.result = result
+        self.rectToBase = rectToBase
+        self.image = image
+        self.id = id != nil ? id! : UUID()
+        self.parentId = parentId
+        if stage == nil {
+            if parentId == nil {
+                self.stage = .generatedNotUpscaled3
+            } else {
+                self.stage = .needsRectToParentResult0
+            }
+        } else {
+            self.stage = stage!
+        }
+    }
+
+    init(rectToBase: CGRect, parent: SSAsset) {
+        self.init(image: nil, result: nil, rectToBase: rectToBase, parentId: parent.id)
+    }
+
+    static func getNodeById(assets: [SSAsset], id: UUID) -> SSAsset? {
+        return assets.first { $0.id == id }
+    }
+
+    // in old baseImage coord space, not new scaled (maxScale) space
+    func getOffsetRelativeToBase(assets: [SSAsset]) -> CGPoint {
+        if self.isBaseImage {
+            return CGPoint(x: 0, y: 0)
+        }
+        // else, have parent
+        guard let register = self.result else {
+            print("Expected to have a parent and a register with offset relative to parent")
+            return CGPoint(x: -1337, y: -1337)
+        }
+        guard let pid = self.parentId else {
+            print("Expected node parentId not to be nil, this should not happen")
+            return CGPoint(x: -1337, y: -1337)
+        }
+        // makes multiple calls to this O(n^2) which is OK for low values of n
+        guard let parent = SSAsset.getNodeById(assets: assets, id: pid) else {
+            print("Expected node to not be nil, this should not happen")
+            return CGPoint(x: -1337, y: -1337)
+        }
+        let scaleParentToBase = parent.getScaleRelativeToBase(assets: assets)
+        var ox = CGFloat(register.offsetX) * scaleParentToBase
+        var oy = CGFloat(register.offsetY) * scaleParentToBase
+        let po = parent.getOffsetRelativeToBase(assets: assets)
+        ox += po.x
+        oy += po.y
+        let offset = CGPoint(x: ox, y: oy)
+        return offset
+    }
+
+    // this is O(n) so the ForEach call is O(n^2) which should not be a problem for under a dozen images, or even higher
+    // really, you shouldn't go for like hundreds of images, as this is untested :sucking air through teeth emoji:
+    func getScaleRelativeToBase(assets: [SSAsset]) -> Double {
+        var node = self
+        var scale = 1.0
+        while !node.isBaseImage {
+            guard let register = node.result else {
+                print("Expected to have a Register result if not a base image")
+                break
+            }
+            scale *= register.scale
+            if let pid = node.parentId,
+                let parent = SSAsset.getNodeById(assets: assets, id: pid)
+            {
+                node = parent
+            } else {
+                print("Expected node to not be nil, this should not happen")
+                break
+            }
+        }
+        return scale
+    }
+}
+
 struct MyShape: Hashable, Equatable, Identifiable {
     let points: [CGPoint]
     let pointsClassification: VNPointsClassification
@@ -54,6 +160,185 @@ struct ImageAnnotations: Hashable, Equatable, Identifiable {
     let boundsShape: MyShape
     let finalRect: CGRect
     let finalShapes: [MyShape]
+    let assets: [SSAsset]
+}
+
+public enum AssetStage {
+    case needsRectToParentResult0
+    case needsCrop1
+    case cropScaled2
+    case generatedNotUpscaled3
+    case finalUpscaled4
+}
+
+class AssetCollection {
+    public var baseImage: CGImage? { assets.count > 0 ? assets[0].image : nil }
+
+    init(assets: [SSAsset]) {
+        self.assets = assets
+    }
+
+    private func computeMaxScale() -> Double {
+        var maxScale = 0.1
+        for asset in assets {
+            let scale = 1.0 / asset.getScaleRelativeToBase(assets: assets)
+            maxScale = max(scale, maxScale)
+        }
+        return maxScale
+    }
+
+    public var assets: [SSAsset] = []
+    var scaleCache: [Int: Double] = [:]
+
+    // cache Max Scale for a given set of unique UUIDs
+    public func getMaxScale() -> Double {
+        let hash: Int = hashAllSets
+        if let ret = scaleCache[hash] {
+            return ret
+        }
+        let result = computeMaxScale()
+        scaleCache[hash] = result
+        return result
+    }
+
+    public func getTrueOffsetRelativeToParent(asset: SSAsset) -> CGPoint {
+        if asset.isBaseImage {
+            return CGPoint(x: 0, y: 0)
+        }
+        guard let pid = asset.parentId else {
+            print("Assets besides baseImage should have a parentId!")
+            return CGPoint(x: -1337, y: -1337)
+        }
+        guard let parent = SSAsset.getNodeById(assets: assets, id: pid) else {
+            print("Assets besides baseImage should have a parent!")
+            return CGPoint(x: -1337, y: -1337)
+        }
+        var pOrigin = CGPoint(x: 0, y: 0)
+        if let parentRectToBase = parent.rectToBase {
+            pOrigin = parentRectToBase.origin
+        }
+        guard let orig = asset.rectToBase?.origin else {
+            print("Expected own rect to be non-null while converting rect to result")
+            return CGPoint(x: -1337, y: -1337)
+        }
+        //let parentOffset = getTrueOffsetRelativeToParent(asset: parent)
+        //let parentOriginToGrand = parentRectToBase.origin
+        let scale = getScaleRelativeToParent(asset: asset)
+        return CGPoint(
+            x: (orig.x - pOrigin.x) / scale,
+            y: (orig.y - pOrigin.y) / scale
+        )
+    }
+
+    public func getScaleRelativeToParent(asset: SSAsset) -> CGFloat {
+        if asset.isBaseImage {
+            return 1.0
+        }
+        guard let pid = asset.parentId else {
+            print("Assets besides baseImage should have a parentId!")
+            return -1337.0
+        }
+        guard let parent = SSAsset.getNodeById(assets: assets, id: pid) else {
+            print("Assets besides baseImage should have a parent!")
+            return -1337.0
+        }
+        var w: CGFloat = CGFloat(baseImage!.width)
+        if let pRect = parent.rectToBase {
+            w = CGFloat(pRect.width)
+        }
+        guard let rect = asset.rectToBase else {
+            print("Expected own rect to be non-null while converting rect to result")
+            return -1337.0
+        }
+        let scale: Double = rect.width / w
+        return scale
+    }
+
+    public func getTrueScale(asset: SSAsset) -> Double {
+        let max = getMaxScale()
+        let scale = asset.getScaleRelativeToBase(assets: assets)
+        // returns 1.0 for smallest image, and maxScale for biggest (base) image
+        return max * scale
+    }
+
+    public func getTrueSize(asset: SSAsset) -> CGSize {
+        guard let image = asset.image else {
+            print("getTrueSize expected asset.image != nil")
+            return CGSize(width: -1, height: -1)
+        }
+        let trueScale = getTrueScale(asset: asset)
+        let bigw = round(trueScale * CGFloat(image.width))
+        let bigh = round(trueScale * CGFloat(image.height))
+        return CGSize(width: bigw, height: bigh)
+    }
+
+    public var hashAllSets: Int {
+        var hasher = Hasher()
+        for asset in assets {
+            asset.hash(into: &hasher)
+        }
+        return hasher.finalize()
+    }
+
+    func verifyParentage(_ asset: SSAsset) -> Bool {
+        var node = asset
+        if node.isBaseImage {
+            //print("isBaseImage: \(node.id)")
+        }
+        while !node.isBaseImage {
+            guard let pid = node.parentId else {
+                print("Assets besides baseImage should have a parentId!")
+                return false
+            }
+            guard let n = SSAsset.getNodeById(assets: assets, id: pid) else {
+                return false
+            }
+            //print("\(node.id) -- parent is --> \(n.id)")
+            node = n
+        }
+        return true
+    }
+
+    func rectToParentResult(asset: SSAsset) -> SSAsset {
+        //getTrueScale(asset: asset) // TODO this is wrong because it needs to be relative to parent!
+        let scale = getScaleRelativeToParent(asset: asset)
+        let offset = getTrueOffsetRelativeToParent(asset: asset)
+        // need correct scale to get correct nw, nh
+        let nw = Int(scale * CGFloat(baseImage!.width))
+        let nh = Int(scale * CGFloat(baseImage!.height))
+        let reg = Register(
+            scale: scale,
+            offsetX: Int(offset.x),
+            offsetY: Int(offset.y),
+            nw: nw, nh: nh
+        )
+        print("offset: \(reg.offsetX), \(reg.offsetY) -- nw x nh: \(nw) x \(nh)")
+        return SSAsset(
+            image: nil,
+            result: reg,
+            parentId: asset.parentId,
+            stage: .needsCrop1,
+            id: asset.id
+        )
+    }
+
+    func testWalk01() -> [SSAsset] {
+        var newAssets: [SSAsset] = []
+        for asset in assets {
+            //print("----\nVerifying parentage")
+            if !verifyParentage(asset) {
+                print("PROBLEM with parentage of asset \(asset.id)")
+            }
+            if asset.stage != .needsRectToParentResult0 {
+                newAssets.append(asset)  // base asset
+            }
+            if asset.stage == .needsRectToParentResult0 {
+                let newAsset = rectToParentResult(asset: asset)
+                newAssets.append(newAsset)
+            }
+        }
+        return newAssets
+    }
 }
 
 // A Mochi Diffusion Project allows a workflow to include:
@@ -246,7 +531,8 @@ public class MDProjectController {
             bounds: CGRect(),
             boundsShape: MyShape.emptyShape(),
             finalRect: CGRect(),
-            finalShapes: [MyShape.emptyShape()]
+            finalShapes: [MyShape.emptyShape()],
+            assets: []
         )
         print("Got an image of size \(cgImage.width) x \(cgImage.height).")
         let imageSize = CGSize(width: cgImage.width, height: cgImage.height)
@@ -435,23 +721,39 @@ public class MDProjectController {
             extraOverlapRect.width * extraOverlapRect.height / (belowRect.width * belowRect.height)
         // now gather up relevant rectangles
         var fShapes: [MyShape] = []
+        var assets: [SSAsset] = []
+        let baseAsset = SSAsset(image: cgImage)
+        assets.append(baseAsset)
+        var comboOrFull = baseAsset
+        var belowOrFull = baseAsset
         if bigEnough {
             if bigOverlapRatio < 0.4 {
                 fShapes.append(
                     MyShape(points: bigBelowRect.toPathPoints(), classification: .openPath)
                 )
+                assets.append(SSAsset(rectToBase: bigBelowRect, parent: baseAsset))
             }
             fShapes.append(MyShape(points: comboRect.toPathPoints(), classification: .openPath))
+            comboOrFull = SSAsset(rectToBase: comboRect, parent: baseAsset)
+            assets.append(comboOrFull)
             if extraOverlapRatio < 0.5 {
                 fShapes.append(MyShape(points: extraRect.toPathPoints(), classification: .openPath))
+                let bigBelow = SSAsset(rectToBase: extraRect, parent: baseAsset)
+                assets.append(bigBelow)
+                belowOrFull = bigBelow
             }
         }
         if bigEnough && overlapRatio < 0.7 {
             fShapes.append(MyShape(points: belowRect.toPathPoints(), classification: .openPath))
+            assets.append(SSAsset(rectToBase: belowRect, parent: belowOrFull))
         }
         fShapes.append(MyShape(points: anotherRect.toPathPoints(), classification: .openPath))
+        assets.append(SSAsset(rectToBase: anotherRect, parent: comboOrFull))
         fShapes.append(MyShape(points: headRect.toPathPoints(), classification: .openPath))
+        let head = SSAsset(rectToBase: headRect, parent: comboOrFull)
+        assets.append(head)
         fShapes.append(MyShape(points: finalRect.toPathPoints(), classification: .openPath))
+        assets.append(SSAsset(rectToBase: finalRect, parent: head))
         return ImageAnnotations(
             shapes: shapes,
             faceRect: faceRect,
@@ -466,7 +768,8 @@ public class MDProjectController {
                 classification: .openPath
             ),
             finalRect: finalRect,
-            finalShapes: fShapes
+            finalShapes: fShapes,
+            assets: assets
         )
     }
 
@@ -561,6 +864,7 @@ public class MDProjectController {
     let baseHeight: Int
     //var projectModel: MDProject
     var anns: ImageAnnotations
+    let assets: AssetCollection
 
     init(
         path: String,
@@ -576,8 +880,10 @@ public class MDProjectController {
         self.generator = generator
         let folderPath = MDProjectController.imagePathToProjectFolder(path)
         self.folderPath = folderPath
-        let path = "\(self.folderPath)/project.json"
+        //let path = "\(self.folderPath)/project.json"
         self.anns = MDProjectController.findAnnotations(cgImage: cgImage)
+        self.assets = AssetCollection(assets: self.anns.assets)
+        self.assets.assets = self.assets.testWalk01()
         store.projectController = self
     }
 
